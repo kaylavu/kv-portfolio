3c00577c5361e72f8ae39ea30eae8f33a76cb956
Kayla Vu <kaylaavu@gmail.com>
2024-02-17 18:35:39 -0800

    add feature to scroll into view with nav header

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,13 +1,24 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import SectionHeading from './section-heading';
 import { projectsData } from '@/lib/data';
 import Project from '@/components/project';
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Projects() {
+  const { ref, inView } = useInView({ threshold: 0.8 });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection('Projects');
+    }
+  }, [inView, setActiveSection, timeOfLastClick]);
+
   return (
-    <section id="projects" className="scroll-mt-28 mb-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
