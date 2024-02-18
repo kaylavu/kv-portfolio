7cf560517a79f65d6b08f7d04780149d0c9e6713
Kayla Vu <kaylaavu@gmail.com>
2024-02-18 12:40:34 -0800

    comment out about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import About from '@/components/about';
+// import About from '@/components/about';
 import Experience from '@/components/experience';
 import Intro from '@/components/intro';
 import Projects from '@/components/projects';
